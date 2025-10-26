f294930543a3d0d8f1e9c2b4781ea20b15a3ebf9
JowennWong <jowennwong@gmail.com>
2025-10-26 20:58:24 +0800

    Add files via upload

---
+++ CodeSnake/MainPageJS.js
@@ -0,0 +1,30 @@
+
+function AlterColor(At)
+{
+	At.style = "color:lime;background-color:black;";
+}
+
+function Revert(At)
+{
+	At.style = "color:black;background-color:lime;";
+}
+
+function AlterFaceBook(At)
+{
+	At.style = "color:white;background-color:#5555FF;";
+}
+
+function AlterLinkedIn(At)
+{
+	At.style = "color:white;background-color:#87CEEB;";
+}
+
+function AlterYoutube(At)
+{
+	At.style = "color:white;background-color:#FF0000;";
+}
+
+function AlterWhatsApp(At)
+{
+	At.style = "color:White;background-color:#33FF33;";
+}
